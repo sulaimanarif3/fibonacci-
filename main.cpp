ce4d5aa72197122b3f92805d2d70ae190dfbc0e7
sulaimanarif3 <50064178+sulaimanarif3@users.noreply.github.com>
2019-05-10 12:46:04 +0430

    fibonachi

--- main.cpp
+++ main.cpp
@@ -4,13 +4,22 @@ using namespace std;
 
 int main()
 {
-    int feet,inches,totalinches;
-    double centimeter;
-    cout<<"enter inches and feets:";
-    cin>>inches>>feet;
-    totalinches=12*inches+feet;
-    cout<<totalinches<<endl;
-    centimeter=totalinches*2.54;
-    cout<<centimeter;
+    int n,i,f1=0,f2=1,f3;
+    cout<<"enter the number of terms of fibonachi series you want"<<endl;
+    cin>> n;
+    cout<<"first "<<n<<" terms of fiboanchi series are :-"<<endl;
+    for(i=0;i<=n;i++)
+    {
+        if(i<=1)
+            f3=i;
+        else
+        {
+            f3=f1+f2;
+            f1=f2;
+            f2=f3;
+        }
+        cout<<f3<<endl;
+    }
     return 0;
 }
+
